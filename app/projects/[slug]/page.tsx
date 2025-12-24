914068cb89ed31674b7676bd944f57a3e3b1a959
Mutiur <mutiur5bb@gmail.com>
2025-12-25 02:21:27 +0600

    kainda dimu

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -2,12 +2,6 @@ import { getProjectBySlug, projectsData } from '@/lib/data';
 import { notFound } from 'next/navigation';
 import ProjectDetailClient from '@/components/projects/ProjectDetailClient';
 
-export async function generateStaticParams() {
-  return projectsData.map((project) => ({
-    slug: project.slug,
-  }));
-}
-
 export default async function ProjectDetailPage({
   params,
 }: {
